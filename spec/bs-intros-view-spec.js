09c0b2a89ca114e5eea630b48b2635a52d676b2a
xuxiaoxin <xiaoxinxu@126.com>
2017-12-19 16:28:52 +0800

    init bs intros

---
+++ spec/bs-intros-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import BsIntrosView from '../lib/bs-intros-view';
+
+describe('BsIntrosView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
